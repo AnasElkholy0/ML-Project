1852d847febfdee40d90171296177f1cd337da4d
Anas Elkholy <anselkholy200@gmail.com>
2026-02-14 05:38:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -137,16 +137,19 @@ model = make_pipeline(
     preprocessor,
     RandomForestClassifier(n_estimators=200, random_state=42)
 )
-
-skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-cv_scores = cross_val_score(model, X, y, cv=skf, scoring='accuracy')
-model.fit(X, y)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y,
     test_size=0.2,
     random_state=42,
     stratify=y
+    
 )
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+cv_scores = cross_val_score(model, X_train, y_train, cv=skf, scoring='accuracy')
+
+print("CV Scores:", cv_scores)
+print("Mean accuracy:", cv_scores.mean())
+
 
 # =========================
 # UI
