618408905d16d0c217aa8e5d01ba8ade7f29813f
Anas Elkholy <anselkholy200@gmail.com>
2026-02-14 05:43:01 +0200

    Add train_test_split import from sklearn

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
